c09312d917cb973cb47700af12ea3a107a47b784
ShadEnmu <sqlencer@mail.ru>
2023-11-15 07:30:35 +0300

    Интеграция в проект Google Firebase для аутентификации и хранения ключей.

--- app/src/main/java/com/xaelence/esignhakaton/di/AppModule.kt
+++ app/src/main/java/com/xaelence/esignhakaton/di/AppModule.kt
@@ -1,9 +1,24 @@
 package com.xaelence.esignhakaton.di
-
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @Provides
+    @Singleton
+    fun provideFirebaseInstance(): FirebaseAuth = Firebase.auth
 
+    @Provides
+    @Singleton
+    fun provideFirebaseDatabase(): FirebaseDatabase = Firebase.database
 }
